00d7fdacc026e4527d262292e6c7a599008cc945
Andrii <andrii.kondratovich@gmail.com>
2024-04-23 17:34:18 +0300

    upd index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,8 @@ import validateService from "./service/validateService";
 console.log("validateService:::", validateService.validateEmail);
 
 // Можна або так:
-import * as apiService from "./service/apiService";
-console.log("apiService:::", apiService);
+// import * as apiService from "./service/apiService";
+// console.log("apiService:::", apiService);
 
 // або так:
 import { fetchAllUsers, fetchById, updateUserById, x as value, y as name } from "./service/apiService";
