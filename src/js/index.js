3793fbcd28562e68db3c0a64662266792f887839
Andrii <andrii.kondratovich@gmail.com>
2024-04-23 17:00:57 +0300

    upd structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
-import validateService from "./js/validateService";
+import validateService from "./service/validateService";
 console.log("validateService:::", validateService.validateEmail);
 
 // Можна або так:
-import * as apiService from "./js/apiService";
+import * as apiService from "./service/apiService";
 console.log("apiService:::", apiService);
 
 // або так:
-import { fetchAllUsers, fetchById, updateUserById, x as value, y as name } from "./js/apiService";
+import { fetchAllUsers, fetchById, updateUserById, x as value, y as name } from "./service/apiService";
 console.log("fetchAllUsers:::", fetchAllUsers);
 console.log("fetchById:::", fetchById);
 console.log("updateUserById:::", updateUserById);
