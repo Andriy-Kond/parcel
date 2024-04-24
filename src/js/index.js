8f64e327a167e3816a4fbdb1d4a8e664dea06505
Andrii <andrii.kondratovich@gmail.com>
2024-04-24 15:15:25 +0300

    upd path

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import * as apiService from "./service/apiService.js";
 console.log("apiService:::", apiService);
 
 // або так:
-import { fetchAllUsers, fetchById, updateUserById, x as value, y as name } from "./service/apiService";
+import { fetchAllUsers, fetchById, updateUserById, x as value, y as name } from "./service/apiService.js";
 console.log("fetchAllUsers:::", fetchAllUsers);
 console.log("fetchById:::", fetchById);
 console.log("updateUserById:::", updateUserById);
